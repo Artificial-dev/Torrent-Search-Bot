5e81ea11706682281371b1d6444c1fd2aa961772
Artificial Thinker <67840255+Artificial-dev@users.noreply.github.com>
2022-08-18 10:51:32 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,6 +4,21 @@ import os
 
 
 class Config(object):
+    
+    SESSION_NAME = "torrent-Search"
+
+    API_ID = 8658860
+
+    API_HASH = "fbb92e01e221f45920e63eb7000e4e38"
+
+    BOT_TOKEN = "5678670362:AAGJZCyW9nK7Oxd6DEHDujPi5nKtJQb2l0c"
+
+    MAX_INLINE_RESULTS = 50
+    
+    
+    
+    
+    
     SESSION_NAME = os.environ.get("SESSION_NAME")
     API_ID = int(os.environ.get("API_ID", 12345678))
     API_HASH = os.environ.get("API_HASH")
