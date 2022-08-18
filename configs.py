c76e5f9a233ba1692e2cc48b689d80de93774553
Artificial Thinker <67840255+Artificial-dev@users.noreply.github.com>
2022-08-18 10:58:11 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ import os
 
 class Config(object):
     
-    SESSION_NAME = "torrent-Search"
+ '''   SESSION_NAME = "torrent-Search"
 
     API_ID = 8658860
 
@@ -13,14 +13,14 @@ class Config(object):
 
     BOT_TOKEN = "5678670362:AAGJZCyW9nK7Oxd6DEHDujPi5nKtJQb2l0c"
 
-    MAX_INLINE_RESULTS = 50
+    MAX_INLINE_RESULTS = 50 '''
     
     
     
     
     
     SESSION_NAME = os.environ.get("SESSION_NAME")
-    API_ID = int(os.environ.get("API_ID", 12345678))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("API_ID", 8658860))
+    API_HASH = os.environ.get("fbb92e01e221f45920e63eb7000e4e38")
+    BOT_TOKEN = os.environ.get("5678670362:AAGJZCyW9nK7Oxd6DEHDujPi5nKtJQb2l0c")
     MAX_INLINE_RESULTS = int(os.environ.get("MAX_INLINE_RESULTS", 50))
